d215a65319fd0d403e88ae4a970cfbb7b72e7f27
marlonchiu <Knight1898@outlook.com>
2019-09-25 21:31:39 +0800

    初始化项目 配置必备环境

---
+++ blog/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+import { Button } from 'antd'
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <Button>Antd 按钮</Button>
+  </div>
+)
+
+export default Home
