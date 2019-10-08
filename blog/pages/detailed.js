e93aba736f2f297e4cca29488bedc2e2ca06eed2
marlonchiu <Knight1898@outlook.com>
2019-10-08 10:19:04 +0800

    blog detailed详细页面布局

--- blog/pages/detailed.js
+++ blog/pages/detailed.js
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
-import { Row, Col } from 'antd'
+import { Row, Col, Icon, Breadcrumb } from 'antd'
 import Header from '../components/Header'
+import Author from '../components/Author'
+import Advert from '../components/Advert'
+import Footer from '../components/Footer'
+import '../static/style/pages/detailed.css'
+
 const Detailed = () => (
   <>
     <Head>
-      <title>Detailed</title>
+      <title>博客详细页面</title>
     </Head>
     <Header />
     <Row className='comm-main' type='flex' justify='center'>
       <Col className='comm-left' xs={24} sm={24} md={16} lg={18} xl={14}>
-        左侧区域
+        <div>
+          <div className='bread-div'>
+            <Breadcrumb>
+              <Breadcrumb.Item><a href='/'>首页</a></Breadcrumb.Item>
+              <Breadcrumb.Item>视频列表</Breadcrumb.Item>
+              <Breadcrumb.Item>XXXXX</Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
+          <div>
+            <div className='detailed-title'>
+            React实战视频教程-技术胖Blog开发(更新08集)
+            </div>
+            <div className='list-icon center'>
+              <span><Icon type='calendar' /> 2019-06-28</span>
+              <span><Icon type='folder' /> 视频教程</span>
+              <span><Icon type='fire' /> 5498人</span>
+            </div>
+            <div className='detailed-context'>
+              详细内容，下节课编写
+            </div>
+          </div>
+        </div>
       </Col>
-      <Col className='comm-box' xs={0} sm={0} md={7} lg={5} xl={4}>
-        右侧区域
+      <Col className='comm-right' xs={0} sm={0} md={7} lg={5} xl={4}>
+        <Author />
+        <Advert />
       </Col>
     </Row>
+    <Footer />
   </>
 )
 
