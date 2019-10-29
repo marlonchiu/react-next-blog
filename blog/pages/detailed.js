34cb9c7f211b45bb6943a3ca44b9dfdfeaf1991a
marlonchiu <Knight1898@outlook.com>
2019-10-29 19:15:51 +0800

    marked + highlight.js 解析详情

--- blog/pages/detailed.js
+++ blog/pages/detailed.js
@@ -6,123 +6,91 @@ import Author from '../components/Author'
 import Advert from '../components/Advert'
 import Footer from '../components/Footer'
 import '../static/style/pages/detailed.css'
-import ReactMarkdown from 'react-markdown'
 import MarkNav from 'markdown-navbar'
 import 'markdown-navbar/dist/navbar.css'
+import marked from 'marked'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/monokai-sublime.css'
+
 import { getArticleByIdRequest } from '../api/request'
 
-const markdown = '# P01:课程介绍和环境搭建\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-   '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n' +
-  '<code>console.log(111)</code> \n\n' +
-  '# p02:来个Hello World 初始Vue3.0\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n' +
-  '***\n\n\n' +
-  '# p03:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n' +
-  '# p04:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n' +
-  '#5 p05:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n' +
-  '# p06:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n' +
-  '# p03:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n' +
-  '# p04:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n' +
-  '#5 p05:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n' +
-  '# p06:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n' +
-  '# p07:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n' +
-  '``` var a=11; ```'
+const Detailed = (props) => {
+  const { content, title, typeName, addTime, view_count } = props
+  const renderer = new marked.Renderer()
 
-const Detailed = () => (
-  <>
-    <Head>
-      <title>博客详细页面</title>
-    </Head>
-    <Header />
-    <Row className='comm-main' type='flex' justify='center'>
-      <Col className='comm-left' xs={24} sm={24} md={16} lg={18} xl={14}>
-        <div>
-          <div className='bread-div'>
-            <Breadcrumb>
-              <Breadcrumb.Item><a href='/'>首页</a></Breadcrumb.Item>
-              <Breadcrumb.Item>视频列表</Breadcrumb.Item>
-              <Breadcrumb.Item>XXXXX</Breadcrumb.Item>
-            </Breadcrumb>
-          </div>
+  marked.setOptions({
+    renderer: renderer,
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    tables: true,
+    breaks: false,
+    smartLists: true,
+    smartypants: false,
+    highlight: function (code) {
+      return hljs.highlightAuto(code).value
+    }
+  })
+
+  const html = marked(content)
+
+  return (
+    <div>
+      <Head>
+        <title>博客详细页面</title>
+      </Head>
+      <Header />
+      <Row className='comm-main' type='flex' justify='center'>
+        <Col className='comm-left' xs={24} sm={24} md={16} lg={18} xl={14}>
           <div>
-            <div className='detailed-title'>
-            React实战视频教程-技术胖Blog开发(更新08集)
+            <div className='bread-div'>
+              <Breadcrumb>
+                <Breadcrumb.Item><a href='/'>首页</a></Breadcrumb.Item>
+                <Breadcrumb.Item>视频列表</Breadcrumb.Item>
+                <Breadcrumb.Item>{title}</Breadcrumb.Item>
+              </Breadcrumb>
             </div>
-            <div className='list-icon center'>
-              <span><Icon type='calendar' /> 2019-06-28</span>
-              <span><Icon type='folder' /> 视频教程</span>
-              <span><Icon type='fire' /> 5498人</span>
+            <div>
+              <div className='detailed-title'>
+                {title}
+              </div>
+              <div className='list-icon center'>
+                <span><Icon type='calendar' />{addTime}</span>
+                <span><Icon type='folder' />{typeName}</span>
+                <span><Icon type='fire' />{view_count}人</span>
+              </div>
+              <div className='detailed-context' dangerouslySetInnerHTML={{ __html: html }} />
             </div>
-            <div className='detailed-context'>
-              <ReactMarkdown
+          </div>
+        </Col>
+        <Col className='comm-right' xs={0} sm={0} md={7} lg={5} xl={4}>
+          <Author />
+          <Advert />
+          <Affix offsetTop={5}>
+            <div className='detailed-nav comm-box'>
+              <div className='nav-title'>
+                <Icon type='book' />
+                文章目录
+              </div>
+              {/* <MarkNav
+                className='article-menu'
                 source={markdown}
-                escapeHtml={false}
-              />
+                ordered={false}
+              /> */}
             </div>
-          </div>
-        </div>
-      </Col>
-      <Col className='comm-right' xs={0} sm={0} md={7} lg={5} xl={4}>
-        <Author />
-        <Advert />
-        <Affix offsetTop={5}>
-          <div className='detailed-nav comm-box'>
-            <div className='nav-title'>
-              <Icon type='book' />
-              文章目录
-            </div>
-            <MarkNav
-              className='article-menu'
-              source={markdown}
-              ordered={false}
-            />
-          </div>
-        </Affix>
-      </Col>
-    </Row>
-    <Footer />
-  </>
-)
+          </Affix>
+        </Col>
+      </Row>
+      <Footer />
+    </div>
+  )
+}
 
 Detailed.getInitialProps = async (context) => {
   const id = context.query.id
   const articleDetail = await new Promise((resolve) => {
     getArticleByIdRequest(id).then(res => {
-      console.log('远程获取数据结果:', res)
+      // console.log('远程获取数据结果:', res)
       resolve(res.data[0])
     })
   })
