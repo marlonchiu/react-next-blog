987257a3088cfceb0729ead05c0ba282e7ccb1b4
marlonchiu <Knight1898@outlook.com>
2019-11-14 10:44:55 +0800

    admin login静态页面

--- admin/src/Pages/Login.js
+++ admin/src/Pages/Login.js
@@ -1,8 +1,47 @@
 import React, { useState } from 'react'
+import { Card, Input, Icon, Button, Spin } from 'antd'
+import '../static/css/Login.css'
+
 function Login() {
+  const [userName, setUserName] = useState('admin')
+  const [password, setPassword] = useState('123456')
+  const [isLoading, setIsLoading] = useState(false)
+
+  // 登录
+  const checkLogin = () => {
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1500)
+  }
+
   return (
-    <div className='login-page'>
-      Login Page
+    <div className='login-wrap'>
+      <Spin tip='Loading...' spinning={isLoading}>
+        <Card className='login-card' title='React Next Blog System' bordered style={{ width: 400 }}>
+          <Input
+            id='userName'
+            value={userName}
+            size='large'
+            placeholder='Enter your username'
+            prefix={<Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />}
+            onChange={(e) => { setUserName(e.target.value) }}
+          />
+          <br />
+          <br />
+          <Input.Password
+            id='password'
+            value={password}
+            size='large'
+            placeholder='Enter your password'
+            prefix={<Icon type='key' style={{ color: 'rgba(0,0,0,.25)' }} />}
+            onChange={(e) => { setPassword(e.target.value) }}
+          />
+          <br />
+          <br />
+          <Button type='primary' size='large' block onClick={checkLogin}> Login in </Button>
+        </Card>
+      </Spin>
     </div>
   )
 }
--- admin/src/index.js
+++ admin/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './App'
-// import Main from './Pages/Main'
+// import App from './App'
+import Main from './Pages/Main'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Main />, document.getElementById('root'))
